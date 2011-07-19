64b0810b02d658ff4c405840847633f3bb6e6357
Neil Williams <neil@reddit.com>
2011-07-18 21:30:03 -0700

    Implement IM command infrastructure.

--- jabber.py
+++ jabber.py
@@ -1,9 +1,16 @@
+from cStringIO import StringIO
+from contextlib import contextmanager
+
 from twisted.words.xish import domish
 from twisted.words.protocols.jabber import xmlstream, client, jid
 from twisted.application import internet
 
 from http import ProtectedResource
 
+COMMANDS = {}
+def command(fn):
+    COMMANDS[fn.__name__] = fn
+    return fn
 
 class BroadcastAlertListener(ProtectedResource):
     isLeaf = True
@@ -35,9 +42,20 @@ class JabberBot(xmlstream.XMPPHandler):
         if not (hasattr(message, "body") and message.body):
             return
 
+        sender = message["from"]
         body = str(message.body)
-        print body
-        self.sendMessage(message['from'], body)
+        split = body.split(' ')
+        command, args = split[0].lower(), split[1:]
+
+        if command not in COMMANDS:
+            self.sendMessage(sender, 'Unknown command. Try "help".')
+            return
+
+        try:
+            fn = COMMANDS[command]
+            fn(self, sender, *args)
+        except:
+            self._detailed_help(sender, command=command, prefix="ERROR")
 
     # methods
     def setAvailable(self):
@@ -50,8 +68,43 @@ class JabberBot(xmlstream.XMPPHandler):
         message.addElement('body', content=content)
         self.send(message)
 
+    def broadcast(self, content):
+        for recipient in self.recipients:
+            self.sendMessage(recipient, content)
+
     def processAlert(self, tag, message):
-        print "tag = %s; message = %s" % (tag, message)
+        self.broadcast(message)
+
+    def _detailed_help(self, sender, command, prefix=None):
+        pass
+
+    @contextmanager
+    def message(self, to):
+        io = StringIO()
+        yield io
+        self.sendMessage(to, io.getvalue())
+
+    # im commands
+    @command
+    def help(self, sender, command=None):
+        "Get information on available commands."
+        if command and command not in COMMANDS:
+            self.sendMessage(sender, "Unknown command '%s'" % command)
+            return
+
+        if command:
+            # send detailed documentation on the specified command
+            self._detailed_help(sender, command)
+        else:
+            # send an overview of available commands
+            with self.message(sender) as m:
+                for command in COMMANDS.itervalues():
+                    print >>m, "*%s* %s" % (command.__name__, command.__doc__)
+
+    @command
+    def wall(self, sender, *rest):
+        "Broadcast a message to all other alert-recipients."
+        self.broadcast(' '.join(rest))
 
 
 def make_service(config, root):
