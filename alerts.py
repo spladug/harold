289d19ee6dcfd010ce91b1956d169f13528d0493
Neil Williams <neil@reddit.com>
2011-07-29 17:52:44 -0700

    Clean up alerter code.

--- alerts.py
+++ alerts.py
@@ -1,5 +1,4 @@
 from http import ProtectedResource
-from plugin import Plugin
 from conf import PluginConfig, Option, tup
 
 
@@ -10,37 +9,40 @@ class AlertsConfig(PluginConfig):
 class BroadcastAlertListener(ProtectedResource):
     isLeaf = True
 
-    def __init__(self, config, http, bot):
+    def __init__(self, http, alerter):
         ProtectedResource.__init__(self, http)
-        self.bot = bot
-        self.config = config
+        self.alerter = alerter
 
-    def _broadcast(self, message):
+    def _handle_request(self, request):
+        tag = request.args['tag'][0]
+        message = request.args['message'][0]
+        self.alerter.alert(tag, message)
+
+
+class Alerter(object):
+    def __init__(self, config, bot):
+        self.config = config
+        self.bot = bot
+
+    def broadcast(self, message):
         for recipient in self.config.recipients:
             self.bot.sendMessage(recipient, message)
 
-    def _handle_request(self, request):
-        #tag = request.args['tag'][0]
-        message = request.args['message'][0]
-        self._broadcast(message)
+    def alert(self, tag, message):
+        self.broadcast(message)
 
-    def wall(self, jabberbot, sender, *message):
+    def wall(self, bot, sender, *message):
         "Broadcast a message to all other alert-recipients."
         short_name = sender.split('@')[0]
-        self._broadcast("<%s> %s" % (short_name, ' '.join(message)))
+        self.broadcast("<%s> %s" % (short_name, ' '.join(message)))
 
 
 def make_plugin(config, http, jabber):
-    p = Plugin()
     alerts_config = AlertsConfig(config)
+    alerter = Alerter(alerts_config, jabber.bot)
 
     # create the http resource
-    alerter = BroadcastAlertListener(alerts_config,
-                                     http,
-                                     jabber.bot)
-    http.root.putChild("alert", alerter)
+    http.root.putChild("alert", BroadcastAlertListener(http, alerter))
 
     # add the wall command
     jabber.register_command(alerter.wall)
-
-    return p
