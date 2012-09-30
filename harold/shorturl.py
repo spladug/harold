8e0e22ffd86400e2e898ac034d8fb60190d34052
Neil Williams <neil@reddit.com>
2012-09-29 19:45:52 -0700

    Use git.io for URL shortening.

--- harold/shorturl.py
+++ harold/shorturl.py
@@ -2,24 +2,24 @@ import urllib
 from collections import deque
 
 from twisted.internet import reactor
-from twisted.internet.protocol import Protocol
-from twisted.internet.defer import Deferred
-from twisted.web.client import Agent, ResponseDone
+from twisted.internet.defer import Deferred, succeed
+from twisted.web.client import Agent
 
 
-class _ResponseCollector(Protocol):
-    def __init__(self, finished):
-        self.finished = finished
-        self.data = []
+class StringProducer(object):
+    def __init__(self, body):
+        self.body = body
+        self.length = len(body)
 
-    def dataReceived(self, bytes):
-        self.data.append(bytes)
+    def startProducing(self, consumer):
+        consumer.write(self.body)
+        return succeed(None)
 
-    def connectionLost(self, reason):
-        if reason.check(ResponseDone):
-            self.finished.callback("".join(self.data))
-        else:
-            self.finished.errback(None)
+    def pauseProducing(self):
+        pass
+
+    def stopProducing(self):
+        pass
 
 
 class UrlShortener(object):
@@ -37,25 +37,24 @@ class UrlShortener(object):
     def _make_short_url(self, long_url):
         self.request_in_flight = True
 
-        encoded = urllib.quote_plus(long_url)
-        api_uri = "http://is.gd/create.php?format=simple&url=%s" % encoded
+        api_uri = "http://git.io/"
+        encoded = urllib.urlencode({"url": long_url})
+        body_producer = StringProducer(encoded)
 
         agent = Agent(reactor)
-        d = agent.request('GET', api_uri)
+        d = agent.request('POST', api_uri, bodyProducer=body_producer)
 
         def onRequestComplete(data):
             self._onRequestComplete()
             return data
 
         def onResponse(response):
-            if response.code != 200:
+            if response.code != 201:
                 onRequestComplete(None)
                 return long_url
 
-            bodyReceived = Deferred()
-            response.deliverBody(_ResponseCollector(bodyReceived))
-            bodyReceived.addBoth(onRequestComplete)
-            return bodyReceived
+            self._onRequestComplete()
+            return response.headers.getRawHeaders("Location")[-1]
         d.addCallback(onResponse)
 
         def onError(failure):
