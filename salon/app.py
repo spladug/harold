91454b7f9522b20afec22bc91d5d880e51a07c37
Neil Williams <neil@reddit.com>
2017-09-05 21:10:51 -0700

    Simplify salon config
    Don't require harold config in the salon so they can be more separate.

--- salon/app.py
+++ salon/app.py
@@ -1,10 +1,6 @@
 import logging
-import os
 
 from flask import Flask
-
-from harold.conf import HaroldConfiguration
-from harold.plugins.database import DatabaseConfig
 
 import flask_github
 
@@ -12,13 +8,9 @@ import flask_github
 app = Flask(__name__)
 
 
-# get the config
-config = HaroldConfiguration(os.environ["HAROLD_CONFIG"])
-_db_config = DatabaseConfig(config)
-app.config["SQLALCHEMY_DATABASE_URI"] = _db_config.connection_string
-
 # load the rest of the config from the salon config file
 # - flask sessions should be configured (at minimum SECRET_KEY should be set)
+# - database url: SQLALCHEMY_DATABASE_URI
 # - github oauth: GITHUB_CLIENT_ID / GITHUB_CLIENT_SECRET
 # - session ttl: MAX_SESSION_AGE (in seconds)
 # - comma-delimited list of github orgs to allow access: GITHUB_ORGS
