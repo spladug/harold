08a872712b67727f7fa9d2594b915dca22e13a19
Neil Williams <neil@reddit.com>
2018-06-08 14:54:39 -0700

    Dehilight with zero-width spaces instead of dots!

--- harold/utils.py
+++ harold/utils.py
@@ -48,7 +48,7 @@ def pretty_and_accurate_time_span(delta):
 def dehilight(name):
     if name.startswith("@"):
         name = name[1:]
-    return "{}.{}".format(name[0], name[1:])
+    return u"{}\u200B{}".format(name[0], name[1:])
 
 
 def constant_time_compare(actual, expected):
