7056d58481da0f0600ed1747a29ddf9a7f158795
Neil Williams <neil@reddit.com>
2018-08-27 16:32:48 -0700

    Move from GitHub OAuth to intranet proxy authentication
    This is meant to be used with oauth2_proxy SSO and a GitHub Enterprise
    installation that uses usernames mapped predictably off SSO usernames.

--- salon/app.py
+++ salon/app.py
@@ -2,8 +2,6 @@ import logging
 
 from flask import Flask
 
-import flask_github
-
 
 app = Flask(__name__)
 
@@ -11,14 +9,9 @@ app = Flask(__name__)
 # load the rest of the config from the salon config file
 # - flask sessions should be configured (at minimum SECRET_KEY should be set)
 # - database url: SQLALCHEMY_DATABASE_URI
-# - github oauth: GITHUB_CLIENT_ID / GITHUB_CLIENT_SECRET
-# - session ttl: MAX_SESSION_AGE (in seconds)
 # - comma-delimited list of github orgs to allow access: GITHUB_ORGS
 # - anything else flask allows to be configured
 app.config.from_envvar("SALON_CONFIG")
-
-# set up flask_github
-github = flask_github.GitHub(app)
 
 # just output to console!
 logging.basicConfig(level=logging.INFO)
--- salon/views.py
+++ salon/views.py
@@ -1,42 +1,15 @@
-import base64
 import collections
-import functools
-import os
-import time
 
-from flask import render_template, session, redirect, request, abort
+from flask import render_template, request, g
 
-from harold.plugins.http import constant_time_compare
-from salon.app import app, github
+from salon.app import app
 from salon.models import db, PullRequest, ReviewState
 
 
-REQUESTED_SCOPES = ", ".join(["read:org"])
-
-
-def _or_list(items):
-    if len(items) == 1:
-        return items[0]
-    return ", ".join(items[:-1]) + " or " + items[-1]
-
-
 @app.before_request
-def csrf_protect():
-    if request.method == "POST":
-        expected = session.pop("_csrf_token", None)
-        actual = request.form.get("_csrf_token")
-
-        if not (expected and constant_time_compare(expected, actual)):
-            abort(400)
-
-
-def make_csrf_token():
-    if "_csrf_token" not in session:
-        session["_csrf_token"] = base64.b64encode(os.urandom(16))
-    return session["_csrf_token"]
-
-
-app.jinja_env.globals["csrf_token"] = make_csrf_token
+def get_username():
+    okta_id = request.headers["Authenticated-User"]
+    g.username = okta_id.partition("@")[0].lower().replace(".", "-")
 
 
 @app.context_processor
@@ -80,132 +53,45 @@ def _categorize_by_states(query):
     return pull_requests
 
 
-@app.route("/login")
-def login():
-    if session.get("username"):
-        return redirect("/")
-
-    # if they've been here before, just go straight to the redirect to save
-    # some effort.
-    if session.get("timestamp"):
-        return github.authorize(REQUESTED_SCOPES)
-
-    return render_template(
-        "login-required.html",
-        organization=_or_list(app.config["GITHUB_ORGS"]),
-    )
-
-
-@app.route("/login", methods=["POST"])
-def post_login():
-    if session.get("username"):
-        return redirect("/")
-    return github.authorize(REQUESTED_SCOPES)
-
-
-@app.route("/logout")
-def logout():
-    if not session.get("username"):
-        return redirect("/")
-    return render_template("logout.html")
-
-
-@app.route("/logout", methods=["POST"])
-def post_logout():
-    if not session.get("username"):
-        return "you must be logged in to log out", 403
-
-    del session["username"]
-    del session["timestamp"]
-    return redirect("/")
-
-
-@app.route("/github-callback")
-@github.authorized_handler
-def authorized(oauth_token):
-    if not oauth_token:
-        return render_template("login-failed.html", reason="not authorized")
-
-    github.access_token_getter(lambda: oauth_token)
-
-    userinfo = github.get("user")
-    username = userinfo["login"]
-
-    orgs = github.get("user/orgs")
-    for org in orgs:
-        if org["login"] in app.config["GITHUB_ORGS"]:
-            break
-    else:
-        reason = "@{username} is not a member of {org}".format(
-            username=username, org=_or_list(app.config["GITHUB_ORGS"]))
-        return render_template("login-failed.html", reason=reason)
-
-    session["username"] = username
-    session["timestamp"] = int(time.time())
-    return redirect("/")
-
-
-def authentication_required(fn):
-    @functools.wraps(fn)
-    def authenticator(*args, **kwargs):
-        timestamp = session.get("timestamp") or 0
-        session_age = time.time() - timestamp
-        if session_age >= app.config["MAX_SESSION_AGE"].total_seconds():
-            try:
-                del session["username"]
-            except KeyError:
-                pass
-            return redirect("/login")
-
-        github_username = session.get("username")
-        if not github_username:
-            return redirect("/login")
-
-        return fn(github_username.lower(), *args, **kwargs)
-    return authenticator
-
-
 @app.route("/")
 @app.route("/user/<override_username>")
-@authentication_required
-def salon(github_username, override_username=None):
+def salon(override_username=None):
+    username = g.username
     if override_username:
-        github_username = override_username.lower()
+        username = override_username.lower()
 
     to_review_query = (
         PullRequest.query
             .options(db.subqueryload(PullRequest.states))
             .filter(PullRequest.state == "open")
-            .filter(db.func.lower(PullRequest.author) != github_username)
+            .filter(db.func.lower(PullRequest.author) != username)
             .join(ReviewState)
-            .filter(db.func.lower(ReviewState.user) == github_username)
+            .filter(db.func.lower(ReviewState.user) == username)
             .order_by(db.desc(PullRequest.created))
     )
     to_review = collections.defaultdict(list)
     for pull_request in to_review_query:
-        state = pull_request.current_states()[github_username]
+        state = pull_request.current_states()[username]
         to_review[state].append(pull_request)
 
     my_pulls_query = (
         PullRequest.query
             .options(db.subqueryload(PullRequest.states))
             .filter(PullRequest.state == "open")
-            .filter(db.func.lower(PullRequest.author) == github_username)
+            .filter(db.func.lower(PullRequest.author) == username)
             .order_by(db.desc(PullRequest.created))
     )
     my_pulls = _categorize_by_states(my_pulls_query)
 
     return render_template(
         "home.html",
-        github_username=github_username,
         my_pulls=my_pulls,
         to_review=to_review,
     )
 
 
 @app.route("/overview")
-@authentication_required
-def overview(github_username):
+def overview():
     query = (
         PullRequest.query
             .options(db.subqueryload(PullRequest.states))
@@ -217,5 +103,4 @@ def overview(github_username):
     return render_template(
         "overview.html",
         pull_requests=pull_requests,
-        github_username=github_username,
     )
