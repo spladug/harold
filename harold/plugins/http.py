6a12ddcdcd64d973139916a25cd09b16b05e3936
Kevin Dious <kevin.dious@reddit.com>
2019-09-19 02:43:03 -0400

    Add HTTP endpoint to retrive all salon names
    Add the /harold/deploy/get_salon_names endpoint
    
    Returns a JSON array containing all salon names

--- harold/plugins/http.py
+++ harold/plugins/http.py
@@ -26,25 +26,38 @@ class ProtectedResource(resource.Resource):
     def __init__(self, http):
         self.http = http
 
+    def _authenticate_request(self, request):
+        HEADER_NAME = "X-Hub-Signature"
+        has_signature = request.requestHeaders.hasHeader(HEADER_NAME)
+        if self.http.hmac_secret and has_signature:
+            # modern method: hmac of request body
+            body = request.content.read()
+            expected_hash = hmac.new(
+                self.http.hmac_secret, body, hashlib.sha1).hexdigest()
+
+            header = request.requestHeaders.getRawHeaders(HEADER_NAME)[0]
+            hashes = urlparse.parse_qs(header)
+            actual_hash = hashes["sha1"][0]
+
+            if not constant_time_compare(expected_hash, actual_hash):
+                raise AuthenticationError
+        else:
+            # no further authentication methods
+            raise AuthenticationError
+
+    def render_GET(self, request):
+        try:
+            self._authenticate_request(request)
+        except AuthenticationError:
+            request.setResponseCode(403)
+        else:
+            self._handle_request(request)
+
+        return ""
+
     def render_POST(self, request):
         try:
-            HEADER_NAME = "X-Hub-Signature"
-            has_signature = request.requestHeaders.hasHeader(HEADER_NAME)
-            if self.http.hmac_secret and has_signature:
-                # modern method: hmac of request body
-                body = request.content.read()
-                expected_hash = hmac.new(
-                    self.http.hmac_secret, body, hashlib.sha1).hexdigest()
-
-                header = request.requestHeaders.getRawHeaders(HEADER_NAME)[0]
-                hashes = urlparse.parse_qs(header)
-                actual_hash = hashes["sha1"][0]
-
-                if not constant_time_compare(expected_hash, actual_hash):
-                    raise AuthenticationError
-            else:
-                # no further authentication methods
-                raise AuthenticationError
+            self._authenticate_request(request)
         except AuthenticationError:
             request.setResponseCode(403)
         else:
